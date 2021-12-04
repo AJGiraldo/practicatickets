8dcaef9dcd1361a6475aaef73f761281c2977cd6
Andres JGB <alixon01344@gmail.com>
2021-12-04 10:42:32 -0500

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChairComponent } from './chair/chair.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+      path:'',//TODO: localhost:4200/ <--
+      component: ChairComponent
+    },
+    {
+      path:'sillas',//TODO: localhost:4200/ <--
+      component: ChairComponent
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChairComponent } from './chair/chair.component';
+import { GeneralComponent } from './general/general.component';
+import { PalletComponent } from './pallet/pallet.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChairComponent,
+    GeneralComponent,
+    PalletComponent
   ],
   imports: [
     BrowserModule,
